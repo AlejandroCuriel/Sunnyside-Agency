768e3d18591412fe11136b44967ad370a623266d
Alejandro Curiel <alex_curielg@hotmail.com>
2021-07-15 23:32:49 -0500

    Configuracion basica para la pagina terminada

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+sass.compiler = require('sass');
+
+const paths = {
+    url_scss: 'src/scss/**/*.scss',
+    url_images: 'src/images/**/*.jpg'
+}
+
+function compilarSass(){
+    return src(paths.url_scss)
+        .pipe( sass())
+        .pipe( dest("./build/css"))
+}
+function minificarImagenes(){
+    return src(paths.url_images)
+        .pipe( imagemin())
+        .pipe( dest('./build/img'))
+        .pipe( notify("Imagen Minificada"))
+}
+function minificarCss(){
+    return src(paths.url_scss)
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe( dest('./build/css'))
+}
+function expandirCss(){
+    return src(paths.url_scss)
+        .pipe( sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe( dest('./build/css'))
+}
+function watchArchivos(){
+    watch(paths.url_scss, compilarSass);
+}
+exports.compilarSass = compilarSass;
+exports.watchArchivos = watchArchivos;
+exports.minificarCss = minificarCss;
+exports.expandirCss = expandirCss;
+exports.minificarImagenes = minificarImagenes;
